b56f6a1308bb0592b24a359dc3f7f2b64c94098e
Timothy Brom <thb@timbrom.com>
2013-03-04 07:04:12 -0500

    Added linker script, openocd debugger and simple infinite loop main program

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#define TOP_OF_STACK 0x20000800
+
+static void nmi_handler(void);
+static void hf_handler(void);
+int main(void);
+
+// Set up the vector table
+unsigned int *vec_table[4] __attribute__ ((section("vectors"))) = {
+    (unsigned int*) TOP_OF_STACK,       // The stack pointer
+    (unsigned int*) main,               // Entry point
+    (unsigned int*) nmi_handler,        // NMI Handler
+    (unsigned int*) hf_handler,         // The hard fault handler
+};
+
+int main(void)
+{
+    int i = 0;
+    for(;;)
+    {
+        i++;
+    }
+}
+
+void nmi_handler(void)
+{
+    for(;;);
+}
+
+void hf_handler(void)
+{
+    for(;;);
+}
+
